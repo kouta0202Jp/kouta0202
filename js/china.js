6fc22b7dbec6f8be36f7c7de79dc508e179c5263
kouta0202Jp <bujitianzhong03@gmail.com>
2025-01-09 16:02:03 +0900

    Create china.js

---
+++ js/china.js
@@ -0,0 +1,17 @@
+// created by KOUTA
+async function checkAccess() {
+    try {
+        const response = await fetch('http://ip-api.com/json/');
+        const data = await response.json();
+
+        // 国コードがCN（中国）であれば404.htmlにリダイレクト
+        if (data.countryCode === 'CN') {
+            window.location.href = '404.html';
+        }
+    } catch (error) {
+        console.error('エラーが発生しました:', error);
+    }
+}
+
+// ページが読み込まれたときに実行
+window.onload = checkAccess;
